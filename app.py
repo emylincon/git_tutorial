ccccbfe299131e0ebf28bbf108dd6c2a5b833c77
391650 <firetiger640@gmail.com>
2022-10-27 13:57:42 +0100

    updated the multiply function

--- app.py
+++ app.py
@@ -7,13 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/multiply/<a>/<b>', methods=['GET'])
-def multiply(a, b):
-    try:
-        a, b = float(a), float(b)
-    except ValueError:
-        return jsonify({'Value Error': "Only numbers Please"})
-    return jsonify({'result': a * b})
 
 @app.route('/multiply/<a>/<b>', methods=['GET'])
 def multiply(a, b):
